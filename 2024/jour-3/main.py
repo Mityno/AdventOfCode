0fde29d10e5cda52081dbf39b5e4642a5da7888b
Titouan Vial <titouan.vial@grenoble-inp.org>
2024-12-03 17:28:54 +0100

    Jour 3 - Partie 2

--- 2024/jour-3/main.py
+++ 2024/jour-3/main.py
@@ -1,48 +1,81 @@
 import re
+import collections
 
 
-def import_sections(filename):
-
+def import_section(filename):
     with open(filename, mode="r", encoding="utf8") as file:
-        sections = file.readlines()
+        section = file.read().strip().replace('\n', '')
 
-    sections = list(map(str.strip, sections))
-    return sections
+    return section
+
+
+def parse_mul(string, index) -> tuple[int, int] | None:
+    mul_regex = r"mul\((\d{1,3}),(\d{1,3})\)"
+    numbers = re.findall(mul_regex, string[index : index + len("mul(XXX,XXX)")])
+    if not numbers:
+        return
+    ab = numbers[0]
+    return tuple(map(int, ab))
 
 
 def main(filename):
+    section = import_section(filename)
 
-    sections = import_sections(filename)
-
-    mul_regex = r"mul\((\d+),(\d+)\)"
-    do_dont_regex = r"(?<=do\(\))(.*?)(?=don't\(\))"
+    mul_regex = r"mul\((\d{1,3}),(\d{1,3})\)"
 
     # Part 1
-    res = 0
-    for section in sections:
-        finds = re.findall(mul_regex, section)
-        nums = [int(a) * int(b) for a, b in finds]
-        res += sum(nums)
+    finds = re.findall(mul_regex, section)
+    nums = [int(a) * int(b) for a, b in finds]
+    res = sum(nums)
 
     print(res)
 
-    re.purge()
+    # Part 2
 
-    # part 2
-    res = 0
-    for section in sections:
-        section = "do()" + section + "don't()"
-        matches = re.findall(do_dont_regex, string=section)
-        print(len(matches))
-        print(matches[0])
+    do_dont_regex = r"(?<=do\(\))(.*?)(?=don't\(\))"
 
-        finds = [re.findall(mul_regex, match) for match in matches]
-        finds = [nums for line in finds for nums in line]
-        print(len(finds))
-        nums = [int(a) * int(b) for a, b in finds]
-        res += sum(nums)
+    section = "do()" + section + "don't()"
+    matches = re.findall(do_dont_regex, string=section)
+    # print(any("don't" in match for match in matches))
+    # print(len(matches))
+    # print(matches[0])
+
+    finds = [re.findall(mul_regex, match) for match in matches]
+    finds = [nums for line in finds for nums in line]
+    # print(len(finds))
+    nums = [int(a) * int(b) for a, b in finds]
+    res = sum(nums)
 
     print(res)
+
+    dont_do_regex = r"don't\(\).*?(?>do\(\)|$)"
+    file = open("result2", mode="w", encoding="utf8")
+
+    do_section = re.sub(dont_do_regex, "", section)
+    file.write(do_section)
+    file.write("\n")
+    # print(do_section)
+    finds = re.findall(mul_regex, do_section)
+    nums = [int(a) * int(b) for a, b in finds]
+    res = sum(nums)
+
+    file.close()
+    print(res)
+
+    total = 0
+    enable = True
+    for i in range(len(section)):
+        if section[i : i + len("do()")] == "do()":
+            enable = True
+        elif section[i : i + len("don't()")] == "don't()":
+            enable = False
+        elif enable and section[i : i + len("mul")] == "mul":
+            numbers = parse_mul(section, i)
+            if numbers is not None:
+                a, b = numbers
+                total += a * b
+
+    print(total)
 
 
 if __name__ == "__main__":
