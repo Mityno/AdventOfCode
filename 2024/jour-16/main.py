63eaabee4735d28f0510922dbc8b555847d2273d
Titouan Vial <titouan.vial@grenoble-inp.org>
2024-12-17 16:26:09 +0100

    Jour 16 - Partie 1 presque finie

--- 2024/jour-16/main.py
+++ 2024/jour-16/main.py
@@ -1,18 +1,33 @@
 import sys
 
-sys.setrecursionlimit(100000)
+# sys.setrecursionlimit(100000)
 
 DIRECTIONS = ((-1, 0), (0, 1), (1, 0), (0, -1))
 
 
-def import_maze(filename: str):
+def import_maze_graph(
+    filename: str,
+) -> tuple[list[list[str]], dict[tuple[int, int], list[tuple[int, int]]]]:
 
     with open(filename, mode="r", encoding="utf8") as file:
         maze = file.read()
 
     maze = maze.strip().split()
     maze = list(map(list, maze))
-    return maze
+    graph_dico: dict[tuple[int, int], list[tuple[int, int]]] = {}
+    n = len(maze)
+    for x in range(n):
+        for y in range(n):
+            if maze[y][x] == "#":
+                continue
+
+            curr_list: list[tuple[int, int]] = []
+            graph_dico[(x, y)] = curr_list
+            for dx, dy in DIRECTIONS:
+                if maze[y + dy][x + dx] != "#":
+                    curr_list.append((x + dx, y + dy))
+
+    return maze, graph_dico
 
 
 def find(maze: list[list[str]], char: str) -> tuple[int, int]:
@@ -24,51 +39,63 @@ def find(maze: list[list[str]], char: str) -> tuple[int, int]:
     raise ValueError(f"{char!r} not in given maze")
 
 
-def solve_min(maze: list[list[str]]) -> int:
-    start_x, start_y = find(maze, "S")
-    end_x, end_y = find(maze, "E")
-    visited = {(start_x, start_y)}
-    path = list(visited)
-    best_score = 100000
+def custom_dijkstra(
+    graph_dico: dict[tuple[int, int], list[tuple[int, int]]],
+    start: tuple[int, int],
+    end: tuple[int, int],
+):
 
-    def inner(pos_x: int, pos_y: int, curr_dir: tuple[int, int], curr_score: int):
-        nonlocal best_score
+    n = len(graph_dico)
+    graph_indices = {
+        summit: index for summit, index in zip(graph_dico.keys(), range(n))
+    }
+    summits = list(graph_dico.keys())
+    unseen: set[tuple[int, int]] = set(graph_dico.keys())
+    distances: list[int] = [float("inf")] * n  # pyright: ignore[reportAssignmentType]
+    prev: list[int] = [0] * n
 
-        if curr_score > best_score:
-            return
+    start_index = graph_indices[start]
+    end_index = graph_indices[end]
+    distances[start_index] = 0
 
-        if (pos_x, pos_y) == (end_x, end_y):
+    while unseen:
+        curr = min(unseen, key=lambda summit: distances[graph_indices[summit]])
+        curr_index = graph_indices[curr]
+        unseen.discard(curr)
 
-            if curr_score < best_score:
-                print('here', curr_score)
-                best_score = curr_score
-                return
+        for neighbor in graph_dico[curr]:
+            neighbor_index = graph_indices[neighbor]
 
-        for dir in DIRECTIONS:
-            dx, dy = dir
-            if (pos_x + dx, pos_y + dy) in visited:
-                continue
+            prev_index = prev[curr_index]
+            prev_summit = summits[prev_index]
+            if curr == start:
+                rotation = (1, 0) != vector_difference(curr, neighbor)  # fmt: skip
+            else:
+                rotation = vector_difference(prev_summit, curr) != vector_difference(curr, neighbor)  # fmt: skip
 
-            if maze[pos_y + dy][pos_x + dx] == "#":
-                continue
+            update_dist(distances, prev, curr_index, neighbor_index, rotation)
 
-            next_score = curr_score + 1 + (curr_dir != dir) * 1000
-            path.append((pos_x + dx, pos_y + dy))
-            visited.add((pos_x + dx, pos_y + dy))
-            inner(pos_x + dx, pos_y + dy, dir, next_score)
-            visited.discard((pos_x + dx, pos_y + dy))
-            _ = path.pop()
+    return distances, start_index, end_index
 
-    inner(start_x, start_y, (0, 1), 0)
-    return best_score
+
+def vector_difference(a: tuple[int, int], b: tuple[int, int]):
+    return a[0] - b[0], a[1] - b[1]
+
+
+def update_dist(distances: list[int], prev: list[int], a: int, b: int, rotation: bool):
+    if distances[b] > distances[a] + 1 + 1000 * rotation:
+        distances[b] = distances[a] + 1 + 1000 * rotation
+        prev[b] = a
 
 
 def main(filename: str):
 
-    maze = import_maze(filename)
+    maze, graph_dico = import_maze_graph(filename)
     print(*map("".join, maze), sep="\n")
-    best_score = solve_min(maze)
-    print(best_score)
+    start = find(maze, "S")
+    end = find(maze, "E")
+    distances, start_index, end_index = custom_dijkstra(graph_dico, start, end)
+    print(distances[end_index])
 
 
 if __name__ == "__main__":
